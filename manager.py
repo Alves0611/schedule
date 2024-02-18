bef01939a0c8468caf3927f3e38d25aefd159116
Alves0611 <g.alves0611@gmail.com>
2024-02-18 13:40:15 -0300

    initial commit

---
+++ manager.py
@@ -0,0 +1,12 @@
+contacts = []
+
+while True:
+    print('\nMenu da Agenda:')
+    print('1. Adicionar contato')
+    print('2. Ver contatos')
+    print('3. Editar contato')
+    print('4. Marcar/Desmarcar como favorito')
+    print('5. Apagar contato')
+    print('6. Sair')
+
+    choice = input('Digite a sua escolha: ')
